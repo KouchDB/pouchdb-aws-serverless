2d5bb1cd0a41349ff92449482111ed71814ca4c1
refacktor <277808+refacktor@users.noreply.github.com>
2022-11-15 21:25:32 -0800

    WIP: convert from API Gateway to Lambda URL

--- pouchdb-sam-app/src/handlers/pouchdb-handler.js
+++ pouchdb-sam-app/src/handlers/pouchdb-handler.js
@@ -9,3 +9,13 @@ const app = express();
 app.use('/', require('express-pouchdb')(PouchDB, {}));
 
 exports.pouchdbServerlessHandler = serverless(app);
+
+// Use something like this for debugging:
+// exports.pouchdbServerlessHandler = serverless(app, {
+//     request(request, event, context) {
+//         console.log(request, event, context);
+//     },
+//     async response(response, event, context) {
+//         console.log(response, event, context);
+//     }
+// });
