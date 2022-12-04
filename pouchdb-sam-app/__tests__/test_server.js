8623f2cf4a87eb458026343d417a2bac8bdd11c8
refacktor <277808+refacktor@users.noreply.github.com>
2022-12-03 19:28:17 -0800

    WIP: try using dynamodbdown (not working)

--- pouchdb-sam-app/__tests__/test_server.js
+++ pouchdb-sam-app/__tests__/test_server.js
@@ -1,8 +1,11 @@
 const levelup = require('levelup');
 const { DynamoDB } = require('aws-sdk');
+
+// WIP (not working):
+//  copied from https://www.npmjs.com/package/dynamodb-leveldown
+//  getting error: TypeError: DynamoDbDown is not a function
 const { DynamoDbDown } = require('dynamodbdown');
 
-// https://www.npmjs.com/package/dynamodb-leveldown
 const factory = DynamoDbDown(new DynamoDB({}));
 const db = levelup(factory('pouchdb'));
 
@@ -10,16 +13,7 @@ const PouchDB = require('pouchdb');
 var CustomPouchDB = PouchDB.defaults({db: db});
 
 const express = require('express');
-const serverless = require('serverless-http');
 const app = express();
-
 app.use('/', require('express-pouchdb')(CustomPouchDB));
 
-exports.pouchdbServerlessHandler = serverless(app, {
-    request(request, event, context) {
-        console.log(request, event, context);
-    },
-    async response(response, event, context) {
-        console.log(response, event, context);
-    }
-});
+app.listen(8080);
